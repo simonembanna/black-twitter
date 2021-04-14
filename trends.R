5f32db5a90ab251e2d15ab589f7931514cc7c6df
simonembanna <82334248+simonembanna@users.noreply.github.com>
2021-04-14 10:17:08 -0400

    Update trends.R

--- trends.R
+++ trends.R
@@ -1,14 +1,20 @@
 # A Trending Topic 0.1
 # author: Simone Banna
 
-library (rtweet)
-library(httpuv)
+# Create Twitter token
+blacktwitterbot_token <- rtweet::create_token(
+  app = "intelftc1",
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
 
-get_token()
-
+# Get Trends
 newyork <- get_trends("new york")
 atl <- get_trends("atlanta")
 chicago <- get_trends("chicago")
 miami <- get_trends("miami")
 
-save_as_csv(newyork, "nyctrends.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+# Save As CSV
+save_as_csv(newyork, "trends.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
